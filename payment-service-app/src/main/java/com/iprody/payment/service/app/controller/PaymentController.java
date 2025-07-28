a1919a6c3dce47aea29acdbb126007f09df323d4
Arina Saray <truffalrimma@gmail.com>
2025-07-28 18:37:22 +0300

    Lesson 8. DB Filtering, Sorting and Pagination

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -1,28 +1,30 @@
 package com.iprody.payment.service.app.controller;
 
 import com.iprody.payment.service.app.persistence.entity.Payment;
+import com.iprody.payment.service.app.persistency.PaymentFilter;
+import com.iprody.payment.service.app.persistency.PaymentFilterFactory;
 import com.iprody.payment.service.app.persistency.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/payments")
+@RequestMapping("/api/payments")
 public class PaymentController {
 
     @Autowired
     private PaymentRepository paymentRepository;
 
     @GetMapping("/{guid}")
-    public ResponseEntity<Payment> getPayment(@PathVariable UUID guid) {
+    public ResponseEntity<Payment> getPaymentByGuid(@PathVariable UUID guid) {
         final Payment payment = paymentRepository.findById(guid)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Payment not found"));
         return ResponseEntity.ok(payment);
@@ -31,5 +33,36 @@ public class PaymentController {
     @GetMapping
     public List<Payment> getPayments() {
         return paymentRepository.findAll();
+    }
+
+    /*
+    @RequestParam - позволяет привязать значение одного GET-параметра из URL запроса к параметру метода обработки.
+    Привязка происходит по имени параметра в методе. Если URL запроса будет таким
+    http://localhost/api/payments/search&page=1, то в параметр метода page попадёт
+    значение 1 из запроса. Аннотация позволяет задавать ряд дополнительных
+    опций, таких как обязательность параметра (required) или значение по умолчанию (defaultValue).
+
+    @ModelAttribute позволяет привязывать GET параметры по именам к полям DTO класса,
+    такого как PaymentFilter, что очень сильно уменьшает объём кода в контроллере.
+    Иначе для каждого поля из класса PaymentFilter нам пришлось бы написать отдельный параметр в методе контроллера.
+     */
+    @GetMapping("/search")
+    public Page<Payment> searchPayments(
+        @ModelAttribute PaymentFilter filter,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "25") int size,
+        @RequestParam(defaultValue = "createdAt") String sortBy,
+        @RequestParam(defaultValue = "desc") String direction
+    ) {
+        //  descending() → Сортировка от большего к меньшему (Z → A, 100 → 1, новые → старые)
+        //  ascending() → Сортировка от меньшего к большему (A → Z, 1 → 100, старые → новые)
+        final Sort sort = direction.equalsIgnoreCase("desc")
+            ? Sort.by(sortBy).descending()
+            : Sort.by(sortBy).ascending();
+
+        return paymentRepository.findAll(
+                PaymentFilterFactory.fromFilter(filter),
+                PageRequest.of(page, size, sort)
+        );
     }
 }
---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistency/PaymentFilterFactory.java
@@ -0,0 +1,48 @@
+package com.iprody.payment.service.app.persistency;
+
+import com.iprody.payment.service.app.persistence.entity.Payment;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+public final class PaymentFilterFactory {
+
+    private static final Specification<Payment> EMPTY = (root, query, cb) -> null;
+
+    public static Specification<Payment> fromFilter(PaymentFilter filter) {
+        Specification<Payment> spec = EMPTY;
+
+        if (StringUtils.hasText(filter.getCurrency())) {
+            spec = spec.and(PaymentSpecifications.hasCurrency(filter.getCurrency()));
+        }
+
+        if (filter.getMinAmount() != null) {
+            spec = spec.and(PaymentSpecifications.minAmount(filter.getMinAmount()));
+        }
+
+        if (filter.getMaxAmount() != null) {
+            spec = spec.and(PaymentSpecifications.maxAmount(filter.getMaxAmount()));
+        }
+
+        if (filter.getMinAmount() != null && filter.getMaxAmount() != null) {
+            spec = spec.and(PaymentSpecifications.amountBetween(filter.getMinAmount(), filter.getMaxAmount()));
+        }
+
+        if (filter.getCreatedBefore() != null) {
+            spec = spec.and(PaymentSpecifications.createdBefore(filter.getCreatedBefore()));
+        }
+
+        if (filter.getCreatedAfter() != null) {
+            spec = spec.and(PaymentSpecifications.createdAfter(filter.getCreatedAfter()));
+        }
+
+        if (filter.getCreatedAfter() != null && filter.getCreatedBefore() != null) {
+            spec = spec.and(PaymentSpecifications.createdBetween(filter.getCreatedAfter(), filter.getCreatedBefore()));
+        }
+
+        if (filter.getStatus() != null) {
+            spec = spec.and(PaymentSpecifications.hasStatus(filter.getStatus()));
+        }
+
+        return spec;
+    }
+}
