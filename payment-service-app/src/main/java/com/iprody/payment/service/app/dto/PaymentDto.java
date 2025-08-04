4c7d55f624a7cdb1dbedc3140253c08439ccc325
Arina Saray <truffalrimma@gmail.com>
2025-08-04 08:41:32 +0300

    Lesson 9. DTO and MapStruct

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/dto/PaymentDto.java
@@ -0,0 +1,95 @@
+package com.iprody.payment.service.app.dto;
+
+import com.iprody.payment.service.app.persistence.entity.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/*
+Класс сущности представляет сущность в формате оптимальном для хранения в БД,
+а DTO в формате удобном для передачи данных между слоями
+ */
+public class PaymentDto {
+    private UUID guid;
+    private UUID inquiryRefId;
+    private BigDecimal amount;
+    private String currency;
+    private UUID transactionRefId;
+    private PaymentStatus status;
+    private String note;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+
+    public UUID getGuid() {
+        return guid;
+    }
+
+    public void setGuid(UUID guid) {
+        this.guid = guid;
+    }
+
+    public UUID getInquiryRefId() {
+        return inquiryRefId;
+    }
+
+    public void setInquiryRefId(UUID inquiryRefId) {
+        this.inquiryRefId = inquiryRefId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public UUID getTransactionRefId() {
+        return transactionRefId;
+    }
+
+    public void setTransactionRefId(UUID transactionRefId) {
+        this.transactionRefId = transactionRefId;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
@@ -0,0 +1,15 @@
+package com.iprody.payment.service.app.service;
+
+import com.iprody.payment.service.app.dto.PaymentDto;
+import com.iprody.payment.service.app.persistence.PaymentFilter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PaymentService {
+    PaymentDto get(UUID id);
+    List<PaymentDto> getAll();
+    Page<PaymentDto> search(PaymentFilter filter, int page, int size,
+        String sortBy, String direction);
+}
